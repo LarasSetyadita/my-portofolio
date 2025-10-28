957d0f4be5c19347f94833107cd6120b630b0b4a
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-10-28 19:25:43 +0700

    feat: complete landing section (full-screen layout)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D9D9D9",
+        secondary: "#263652",
+        accent: "#B77E23"
+      }
+    },
+  },
+  plugins: [],
+}
+
